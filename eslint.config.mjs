d0fd70d37f82903c596510dc4c06cbae2a4e1a0b
roushan-sheik <roushansheik@gmail.com>
2024-10-03 20:32:34 +0600

    add: final node js  setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,28 @@
-# Ignore artifacts:
-build
-coverage
+import eslint from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+      "prefer-const": "error",
+      "no-console": "warn",
+      "no-var": "error",
+      "no-mixed-spaces-and-tabs": "error",
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    ignores: ["**/node_modules/", "**/dist/"],
+  }
+);
