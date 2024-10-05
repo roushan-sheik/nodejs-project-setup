4ef28d246e5fd37700f41ba44ddff4f3768985f4
roushan-sheik <roushansheik@gmail.com>
2024-10-05 07:27:20 +0600

    add: trycatch error handler

--- src/app/modules/Movies/movie.controller.ts
+++ src/app/modules/Movies/movie.controller.ts
@@ -2,37 +2,58 @@ import { Request, Response } from "express";
 import { MovieService } from "./movie.service";
 import { StatusCodes } from "http-status-codes";
 
-// create movie controller
+// create movie controller ====================>
 const createMovie = async (req: Request, res: Response) => {
-  const movieData = req.body;
-  const movie = await MovieService.createMovie(movieData);
+  try {
+    const movieData = req.body;
+    const movie = await MovieService.createMovie(movieData);
 
-  res.status(StatusCodes.CREATED).json({
-    success: true,
-    message: "Movie is created Successfully.",
-    data: movie,
-  });
+    res.status(StatusCodes.CREATED).json({
+      success: true,
+      message: "Movie is created Successfully.",
+      data: movie,
+    });
+  } catch (error) {
+    const err = error as Error;
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: err.message });
+  }
 };
-// get all movies controller
+// get all movies controller ====================>
 const getAllMovie = async (req: Request, res: Response) => {
-  const result = await MovieService.getAllMovie();
+  try {
+    const result = await MovieService.getAllMovie();
 
-  res.status(StatusCodes.OK).json({
-    success: true,
-    message: "All Movies",
-    data: result,
-  });
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: "All Movies",
+      data: result,
+    });
+  } catch (error) {
+    const err = error as Error;
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: err.message });
+  }
 };
-// create movie controller
+// create movie controller ====================>
 const getMovieById = async (req: Request, res: Response) => {
-  const { movieId } = req.params;
-  const result = await MovieService.getMovieById(movieId);
+  try {
+    const { movieId } = req.params;
+    const result = await MovieService.getMovieById(movieId);
 
-  res.status(StatusCodes.OK).json({
-    success: true,
-    message: "Found this movie by Id",
-    data: result,
-  });
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: "Found this movie by Id",
+      data: result,
+    });
+  } catch (error) {
+    const err = error as Error;
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: err.message });
+  }
 };
 
 export const MovieController = { createMovie, getAllMovie, getMovieById };
