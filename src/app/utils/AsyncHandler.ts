80bdf2daa077745821998145835eaf3b2cfcc030
roushan-sheik <roushansheik@gmail.com>
2024-10-08 12:34:22 +0600

    add: custom error class

--- src/app/utils/AsyncHandler.ts
+++ src/app/utils/AsyncHandler.ts
@@ -1,14 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unsafe-function-type */
 import { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
+import CustomError from "./CustomError";
 
 const AsyncHandler = (fn: Function) => {
   return (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch((error) => {
-      const err = error as Error;
-      res
-        .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({ message: err.message, catchFrom: "AsyncHandler" });
+      if (error instanceof CustomError) {
+        res
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json(error.toResponseFormat());
+      }
     });
   };
 };
---
+++ src/app/utils/CustomError.ts
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+class CustomError extends Error {
+  public success: boolean;
+  public timeStamp: string;
+
+  constructor(
+    public statusCode: number,
+    public message: string = "Internal Server Error!!",
+    public errors: string[] | any[] = [],
+    public data: any = undefined,
+    public errorType: string = "General",
+    public stack: string = ""
+  ) {
+    super(message);
+
+    this.statusCode = statusCode;
+    this.success = false; // by default false
+    this.errors = errors;
+    this.data = data;
+    this.timeStamp = new Date().toISOString();
+    this.errorType = errorType;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+
+  //  format the error
+  public toResponseFormat() {
+    return {
+      statusCode: this.statusCode,
+      success: this.success,
+      message: this.message,
+      errors: this.errors,
+      data: this.data,
+      timestamp: this.timeStamp,
+      errorType: this.errorType,
+      stack: process.env.NODE_ENV === "production" ? undefined : this.stack, // প্রডাকশনে স্ট্যাক না দেখানো
+    };
+  }
+}
+
+export default CustomError;
