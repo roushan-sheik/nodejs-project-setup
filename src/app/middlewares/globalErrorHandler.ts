2146e8146bcbaf5ad49d79955452a5924196d0b5
roushan-sheik <roushansheik@gmail.com>
2024-10-13 10:42:21 +0600

    added: error handling and validation

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -9,8 +9,11 @@ import handleValidationError from "../errors/handleValidationError";
 import handleCastError from "../errors/handleCastError";
 import { handleDuplicateError } from "../errors";
 import { handleZodError } from "../errors/handleZodError";
+import ApiError from "../utils/ApiError";
+import GlobalErrorObj from "../utils/GlobalErrorObj";
 
 const globalErrorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+  //? <====================|| Handle ApiError ||===================>
   let customGlobalError: ICustomGlobalError = {
     success: err.success || false,
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
@@ -22,18 +25,24 @@ const globalErrorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
       },
     ],
   };
-  // ValidationError
+
+  //? <====================|| Handle All The Errors ||===================>
   if (err.name === "ValidationError") {
     customGlobalError = handleValidationError(err);
-  } else if (err.name === "CastError") {
+  } else if (err?.name === "CastError") {
     customGlobalError = handleCastError(err);
-  } else if (err.code === 11000) {
+  } else if (err?.code === 11000) {
     customGlobalError = handleDuplicateError(err)!;
-  } else if (err.name === "ZodError") {
+  } else if (err?.name === "ZodError") {
     customGlobalError = handleZodError(err);
+  } else if (err instanceof Error) {
+    customGlobalError = new GlobalErrorObj(
+      500,
+      err?.message || customGlobalError.message,
+      customGlobalError.errorSources
+    );
   }
-
-  // finally send the response
+  //? <====================|| finally send the response ||===================>
   res.status(customGlobalError.statusCode).json({
     success: customGlobalError.success,
     message: customGlobalError.message,
