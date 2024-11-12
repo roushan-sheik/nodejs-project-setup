91b006188f5293dbdada084543f28a5fcb12ef11
roushan-sheik <roushansheik@gmail.com>
2024-11-12 16:17:02 +0600

    add: user interface

--- src/app/modules/User/user.interface.ts
+++ src/app/modules/User/user.interface.ts
@@ -1,1 +1,10 @@
+import { USER_ROLE, USER_STATUS } from "./user.constant";
 
+export type TUser = {
+  name: string;
+  role: keyof typeof USER_ROLE;
+  email: string;
+  password: string;
+  status: keyof typeof USER_STATUS;
+  passwordChangedAt?: Date;
+};
